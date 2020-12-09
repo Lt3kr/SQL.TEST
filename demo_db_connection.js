f0eb8677828a64a0005e376ca6053aeef4524297
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-12-09 09:19:27 +0100

    gjorde test på databas

---
+++ demo_db_connection.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query
+});
+
